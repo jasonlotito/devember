d31d80f4e5d53a09eb7e51ba343d0bcd65c51d04
Jason Lotito <jasonlotito@gmail.com>
2016-12-09 00:16:35 -0500

    Devember 8 - Style, and complete move to React
    Spent more time learning React today. Cleaned things up, and generally
    just continued working with it.  I've effectively started work on
    something else (learning React) instead of what I set out to do, but
    considering what I set out to do in the first place was different from
    what I originally had in mind... I don't really mind.
    
    It's a journey, a discovery of knowledge, and I'm having fun.

--- public/js/main.js
+++ public/js/main.js
@@ -1,50 +1,133 @@
+function ParamList(props) {
+    const paramList = props.paramList;
+    const paramListItems = Object.keys(paramList).map(paramName => {
+        const param = paramList[paramName];
+        return (
+            <ul>
+                <li>Param Name: {paramName}</li>
+                <li>Type: {param.type}</li>
+                <li>Description: {param.description}</li>
+                <li>Default Value: {param.default}</li>
+            </ul>)
+    });
+
+    return (<span>{paramListItems}</span>);
+}
+
 let CommandItem = React.createClass({
     getInitialState: function () {
         return {
             data: {
                 command: '',
-                description: ''
+                description: '',
+                input: {
+                    params: []
+                },
+                output: {
+                    type: ''
+                }
             }
         }
     },
-    componentDidMount: function() {
+    componentDidMount: function () {
         console.log(this)
         $.getJSON('/command/' + this.props.params.command, (data) => {
             console.log(data);
-            this.setState({data:data})
+            this.setState({data: data})
         });
     },
     render: function () {
-        console.log(this.state);
         return (
-        <div>
-            <h1>Command: {this.props.params.command}</h1>
-            <h2>Description</h2>
-            <p>{this.state.data.description ? this.state.data.description : 'Description not found.'}</p>
-
-        </div>)
+            <div>
+                <h1>Command: {this.props.params.command}</h1>
+                <h2>Description</h2>
+                <p>{this.state.data.description ? this.state.data.description : 'Description not found.'}</p>
+                <h2>Parameters</h2>
+                <ParamList paramList={this.state.data.input.params}/>
+                <h2>Output</h2>
+                <p>{this.state.data.output.type}</p>
+            </div>)
     }
 });
+
+function RouteInputItem(data) {
+    const route = data.route;
+    return (
+        <ul>
+            <li>Source: {route.source}</li>
+            <li>Key: {route.key}</li>
+            <li>Param: {route.param}</li>
+        </ul>
+    );
+}
+
+function RouteInputItemList(props) {
+    console.log(props);
+    const routeInput = props.routeInput.map((routeInputs, k) => {
+        const commandName = props.commands[k];
+
+        const inputLists = routeInputs.map((route, keyId) => {
+            console.log(route);
+            return (
+                <ul>
+                    <li>
+                        Param {keyId}
+                        <RouteInputItem route={route}/>
+                    </li>
+                </ul>
+            )
+        });
+
+        const href = `/command/${commandName}`;
+        return (<ul key={k}>
+            <li><Link to={href}>{commandName}</Link> {inputLists}</li>
+        </ul>)
+    });
+
+    return (<div>{routeInput}</div>)
+}
+
+function CommandListItem(data) {
+    console.log(data);
+    const commandList = data.commandList.map( command => {
+        const href = `/command/${command}`;
+        return (<li><Link to={href}>{command}</Link></li>);
+    });
+
+    return (
+        <ul className="route-command-list">{commandList}</ul>
+    )
+}
 
 let RouteItem = React.createClass({
     getInitialState: function () {
         return {
-            data: {}
+            data: {
+                route: '',
+                command: [],
+                commandString: '',
+                routeInput: []
+            }
         }
     },
-    componentDidMount: function() {
-
+    componentDidMount: function () {
+        $.getJSON('/route/' + encodeURIComponent(this.props.params.route), data => {
+            this.setState({data: data});
+        });
     },
     render: function () {
         return (
-        <div>
-            <h1>Route</h1>
-        </div>)
+            <div>
+                <h1>Route: {this.state.data.route}</h1>
+                <p>Commands: <CommandListItem commandList={this.state.data.command}/></p>
+                <RouteInputItemList routeInput={this.state.data.routeInput} commands={this.state.data.command}/>
+            </div>
+        )
     }
 });
 
 let CommandList = React.createClass({
-    getInitialState: function() {
+    getInitialState: function () {
         return {
             data: []
         }
@@ -54,7 +137,7 @@ let CommandList = React.createClass({
             this.setState({data})
         });
     },
-    handleClick: function(e) {
+    handleClick: function (e) {
         // e.preventDefault();
     },
     render: function () {
@@ -103,12 +186,14 @@ let RouteList = React.createClass({
 let Page = React.createClass({
     render: function () {
         return (
-            <div>
-                <h1></h1>
+            <div className="homePage">
+                <h1>Home</h1>
+                <nav>
                 <h2>Commands</h2>
                 <CommandList/>
                 <h2>Routes</h2>
                 <RouteList/>
+                </nav>
             </div>
         )
     }
@@ -123,18 +208,21 @@ let App = React.createClass({
     }
 });
 
-var { Router,
+var {
+    browserHistory,
+    Router,
     Route,
     IndexRoute,
     IndexLink,
-    Link } = ReactRouter;
+    Link
+} = ReactRouter;
 
 ReactDOM.render(
-    <Router>
+    <Router history={browserHistory}>
         <Route path="/" component={App}>
             <IndexRoute component={Page}/>
-            <Route path="route/:route" component={RouteItem} />
-            <Route path="command/:command" component={CommandItem} />
+            <Route path="Route/:route" component={RouteItem}/>
+            <Route path="Command/:command" component={CommandItem}/>
         </Route>
     </Router>,
     document.getElementById('content')
